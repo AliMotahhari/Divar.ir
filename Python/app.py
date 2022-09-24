f44f8c48f87aa55be25992e82a6706806aec18d8
ali mth <99986417+alimotahhari@users.noreply.github.com>
2022-09-24 06:06:45 -0700

    Update app.py

--- Python/app.py
+++ Python/app.py
@@ -1,9 +1,12 @@
 from divar.app import app 
 
+#divar
+
 
 import divar.models as models
 models.db.create_all()
 models.db.create_all()
+#//////////////////////////////////////
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",debug=True,port=8080)
